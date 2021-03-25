b73ad48161b29cfa8dd5d4cd08cb16a4266c4002
karmaskool <karmann.jerome.tech@gmail.com>
2021-03-25 09:16:35 +0100

    service-registry-eureka-server

---
+++ com/impact/karma/demo/java/service-registry-eureka-server/src/test/java/com/impact/karma/service/registry/eureka/server/ServiceRegistryEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.karma.service.registry.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
