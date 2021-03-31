8395e9aafa3f8b07470e590954f033a6ff99b6b3
karmaskool <karmann.jerome.tech@gmail.com>
2021-03-31 18:39:41 +0200

    [Kafka] kafka-messaging-consumer init project

---
+++ com/impact/karma/demo/java/messaging-kafka-consumer/src/main/java/com/impact/karma/messaging/kafka/consumer/MessagingKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.impact.karma.messaging.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingKafkaConsumerApplication.class, args);
+	}
+
+}
