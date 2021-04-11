7a0116aae61f1abb592cd959c73323181126da4a
karmaskool <karmann.jerome.tech@gmail.com>
2021-04-11 09:52:05 +0200

    cloud-config-server

--- com/impact/karma/demo/java/cloud-config-server/src/main/java/com/impact/karma/service/cloud/config/server/CloudConfigServerApplication.java
+++ com/impact/karma/demo/java/cloud-config-server/src/main/java/com/impact/karma/service/cloud/config/server/CloudConfigServerApplication.java
@@ -1,17 +1,21 @@
 package com.impact.karma.service.cloud.config.server;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
 public class CloudConfigServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CloudConfigServerApplication.class, args);
+	
+	public static void main(String[] args) throws IOException {
+		ConfigurableApplicationContext app = SpringApplication.run(CloudConfigServerApplication.class, args);
+		app.getBean(CloudConfigServerConfiguration.class).printConfig();
 	}
 
 }
