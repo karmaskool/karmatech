01ebc491a1e68ccff0bd048ccf59030917ea2e32
karmaskool <karmann.jerome.tech@gmail.com>
2021-04-13 15:22:47 +0200

    @EnableDiscoveryClient // @EnableEurekaClient is more specific
    Signed-off-by: karmaskool <karmann.jerome.tech@gmail.com>

--- com/impact/karma/demo/java/cloud-gateway-api-service/src/test/java/com/impact/karma/service/cloud/gateway/api/service/CloudGatewayApiServiceApplicationTests.java
+++ com/impact/karma/demo/java/cloud-gateway-api-service/src/test/java/com/impact/karma/service/cloud/gateway/api/service/CloudGatewayApiServiceApplicationTests.java
@@ -2,10 +2,10 @@ package com.impact.karma.service.cloud.gateway.api.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootTest
-@EnableEurekaClient
+@EnableDiscoveryClient // @EnableEurekaClient is more specific
 class CloudGatewayApiServiceApplicationTests {
 
 	@Test
--- com/impact/karma/demo/java/messaging-kafka-producer/src/main/java/com/impact/karma/messaging/kafka/producer/MessagingKafkaProducerApplication.java
+++ com/impact/karma/demo/java/messaging-kafka-producer/src/main/java/com/impact/karma/messaging/kafka/producer/MessagingKafkaProducerApplication.java
@@ -2,10 +2,10 @@ package com.impact.karma.messaging.kafka.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient // @EnableEurekaClient is more specific
 public class MessagingKafkaProducerApplication {
 
 	public static void main(String[] args) {
