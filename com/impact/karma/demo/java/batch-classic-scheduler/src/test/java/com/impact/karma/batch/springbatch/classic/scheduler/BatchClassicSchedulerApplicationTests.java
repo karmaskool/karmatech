23ef6a4beef31897565a95493f96bdfcd4ba7ff4
karmaskool <karmann.jerome.tech@gmail.com>
2021-04-14 17:04:48 +0200

    batch-classic-scheduler
    @EnableScheduling @Scheduled

---
+++ com/impact/karma/demo/java/batch-classic-scheduler/src/test/java/com/impact/karma/batch/springbatch/classic/scheduler/BatchClassicSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.karma.batch.springbatch.classic.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchClassicSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
